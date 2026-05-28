aeb1f2e5a52a6edb39d64a687005646e88750ec0
sedina-imamovic <Sedina.Imamovic@iths.se>
2026-05-28 17:17:09 +0200

    initial commit

--- src/main/java/bih/iths/sedina/mfasecurity/security/TwoFactorFilter.java
+++ src/main/java/bih/iths/sedina/mfasecurity/security/TwoFactorFilter.java
@@ -16,10 +16,8 @@ import java.io.IOException;
 public class TwoFactorFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain)
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain)
             throws ServletException, IOException {
 
         String path = request.getRequestURI();
